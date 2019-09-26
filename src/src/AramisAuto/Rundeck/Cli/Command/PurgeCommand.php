68b45091a92a3ef697682bb26cd7ea7fbe5fc5d7
Erfindel <gaucher.ghislain@gmail.com>
2019-09-26 18:06:41 +0200

    SP-1691 fix script to clean Rundeck

--- src/src/AramisAuto/Rundeck/Cli/Command/PurgeCommand.php
+++ src/src/AramisAuto/Rundeck/Cli/Command/PurgeCommand.php
@@ -2,7 +2,6 @@
 
 namespace AramisAuto\Rundeck\Cli\Command;
 
-use mysql_xdevapi\Exception;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Command\Command;
